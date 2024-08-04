38b2c6eaba234272a285b9bd3c7414347623dda1
AuliyaIkhsanaNasution <auliyanasti06@gmail.com>
2024-08-04 11:26:48 +0700

    update : ui news

--- resources/views/pages/news.blade.php
+++ resources/views/pages/news.blade.php
@@ -4,74 +4,130 @@
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt, accusantium!
         </x-elements.header_center>
 
-        <div class="flex flex-col md:flex-row justify-center md:space-x-4 mt-6 space-y-4 md:space-y-0">
-            <a href="#category1"
-                class="block bg-secondary rounded-lg p-6 w-full md:w-1/5 text-center shadow-md hover:bg-gray-300">
-                <h2 class="text-xl font-bold">Pemrograman</h2>
+        <div class="flex flex-row  justify-center items-center mt-6 drop-shadow-xl ">
+
+            <a href="">
+                <div class="flex flex-col items-center mx-2 md:mx-5 ">
+                    <div
+                        class="bg-secondary rounded-full p-6 flex items-center justify-center w-24 h-24  hover:bg-gray-300 hover:scale-125 transition-all">
+                        <svg class="w-12 h-12 text-primary" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor" viewBox="0 0 24 24">
+                            <path fill-rule="evenodd"
+                                d="M12 2c-.791 0-1.55.314-2.11.874l-.893.893a.985.985 0 0 1-.696.288H7.04A2.984 2.984 0 0 0 4.055 7.04v1.262a.986.986 0 0 1-.288.696l-.893.893a2.984 2.984 0 0 0 0 4.22l.893.893a.985.985 0 0 1 .288.696v1.262a2.984 2.984 0 0 0 2.984 2.984h1.262c.261 0 .512.104.696.288l.893.893a2.984 2.984 0 0 0 4.22 0l.893-.893a.985.985 0 0 1 .696-.288h1.262a2.984 2.984 0 0 0 2.984-2.984V15.7c0-.261.104-.512.288-.696l.893-.893a2.984 2.984 0 0 0 0-4.22l-.893-.893a.985.985 0 0 1-.288-.696V7.04a2.984 2.984 0 0 0-2.984-2.984h-1.262a.985.985 0 0 1-.696-.288l-.893-.893A2.984 2.984 0 0 0 12 2Zm3.683 7.73a1 1 0 1 0-1.414-1.413l-4.253 4.253-1.277-1.277a1 1 0 0 0-1.415 1.414l1.985 1.984a1 1 0 0 0 1.414 0l4.96-4.96Z"
+                                clip-rule="evenodd" />
+                        </svg>
+                    </div>
+                    <h2 class="text-xl font-bold mt-4 hover:text-primary">Pemrograman</h2>
+                </div>
             </a>
-            <a href="#category2"
-                class="block bg-secondary rounded-lg p-6 w-full md:w-1/5 text-center shadow-md hover:bg-gray-300">
-                <h2 class="text-xl font-bold">Pemrograman</h2>
+
+            <a href="">
+                <div class="flex flex-col items-center mx-2 md:mx-5 ">
+                    <div
+                        class="bg-secondary rounded-full p-6 flex items-center justify-center w-24 h-24  hover:bg-gray-300 hover:scale-125 transition-all">
+                        <svg class="w-12 h-12 text-primary" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor" viewBox="0 0 24 24">
+                            <path fill-rule="evenodd"
+                                d="M12 2c-.791 0-1.55.314-2.11.874l-.893.893a.985.985 0 0 1-.696.288H7.04A2.984 2.984 0 0 0 4.055 7.04v1.262a.986.986 0 0 1-.288.696l-.893.893a2.984 2.984 0 0 0 0 4.22l.893.893a.985.985 0 0 1 .288.696v1.262a2.984 2.984 0 0 0 2.984 2.984h1.262c.261 0 .512.104.696.288l.893.893a2.984 2.984 0 0 0 4.22 0l.893-.893a.985.985 0 0 1 .696-.288h1.262a2.984 2.984 0 0 0 2.984-2.984V15.7c0-.261.104-.512.288-.696l.893-.893a2.984 2.984 0 0 0 0-4.22l-.893-.893a.985.985 0 0 1-.288-.696V7.04a2.984 2.984 0 0 0-2.984-2.984h-1.262a.985.985 0 0 1-.696-.288l-.893-.893A2.984 2.984 0 0 0 12 2Zm3.683 7.73a1 1 0 1 0-1.414-1.413l-4.253 4.253-1.277-1.277a1 1 0 0 0-1.415 1.414l1.985 1.984a1 1 0 0 0 1.414 0l4.96-4.96Z"
+                                clip-rule="evenodd" />
+                        </svg>
+                    </div>
+                    <h2 class="text-xl font-bold mt-4 hover:text-primary">UI/UX</h2>
+                </div>
             </a>
-            <a href="#category3"
-                class="block bg-secondary rounded-lg p-6 w-full md:w-1/5 text-center shadow-md hover:bg-gray-300">
-                <h2 class="text-xl font-bold">Pemrograman</h2>
+
+            <a href="">
+                <div class="flex flex-col items-center mx-2 md:mx-5 ">
+                    <div
+                        class="bg-secondary rounded-full p-6 flex items-center justify-center w-24 h-24  hover:bg-gray-300 hover:scale-125 transition-all">
+                        <svg class="w-12 h-12 text-primary" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor" viewBox="0 0 24 24">
+                            <path fill-rule="evenodd"
+                                d="M12 2c-.791 0-1.55.314-2.11.874l-.893.893a.985.985 0 0 1-.696.288H7.04A2.984 2.984 0 0 0 4.055 7.04v1.262a.986.986 0 0 1-.288.696l-.893.893a2.984 2.984 0 0 0 0 4.22l.893.893a.985.985 0 0 1 .288.696v1.262a2.984 2.984 0 0 0 2.984 2.984h1.262c.261 0 .512.104.696.288l.893.893a2.984 2.984 0 0 0 4.22 0l.893-.893a.985.985 0 0 1 .696-.288h1.262a2.984 2.984 0 0 0 2.984-2.984V15.7c0-.261.104-.512.288-.696l.893-.893a2.984 2.984 0 0 0 0-4.22l-.893-.893a.985.985 0 0 1-.288-.696V7.04a2.984 2.984 0 0 0-2.984-2.984h-1.262a.985.985 0 0 1-.696-.288l-.893-.893A2.984 2.984 0 0 0 12 2Zm3.683 7.73a1 1 0 1 0-1.414-1.413l-4.253 4.253-1.277-1.277a1 1 0 0 0-1.415 1.414l1.985 1.984a1 1 0 0 0 1.414 0l4.96-4.96Z"
+                                clip-rule="evenodd" />
+                        </svg>
+                    </div>
+                    <h2 class="text-xl font-bold mt-4 hover:text-primary">Pemrograman</h2>
+                </div>
             </a>
+
+
         </div>
     </x-elements.section_template>
 
+
     <section class="bg-secondary rounded-t-3xl p-3 w-full flex justify-center">
         <x-elements.section_template id="News">
+
             <div class="flex mb-6">
                 <div class="w-1/3">
                     <img src="/assets/images/helloTech.webp" alt="News Image 1"
-                        class="object-cover w-full rounded-t-lg max-h-40 md:max-h-36">
+                        class="object-cover w-full rounded-lg max-h-40 md:max-h-36">
                 </div>
                 <div class="w-full pl-3">
-                    <div class="text-sm md:text-base md:pb-1 text-primary">Pemrograman</div>
-                    <h2 class="text-base md:text-xl font-bold">Judul Berita 1</h2>
-                    <p class="text-base md:text-lg md:pb-2">Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    <span
+                        class="bg-blue-100 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded md:mb-3">Pemrograman</span>
+                    <h2 class="text-2xl md:text-3xl font-bold">Judul Berita 1</h2>
+                    <p class="text-sm md:text-base md:pb-3 text-green-800">Lorem ipsum, dolor sit amet consectetur
+                        adipisicing elit.
                         Repudiandae
                         ratione
                         accusantium vel? Autem assumenda eius nobis dolorem quos voluptates tempora.</p>
-                    <div class="text-sm text-primary">Dipublikasikan pada 01 Januari 2024</div>
+                    <div class="text-sm text-primary ">7 menit yang lalu
+                    </div>
                 </div>
             </div>
 
             <div class="flex mb-6">
                 <div class="w-1/3">
                     <img src="/assets/images/helloTech.webp" alt="News Image 1"
-                        class="object-cover w-full rounded-t-lg max-h-40 md:max-h-36">
+                        class="object-cover w-full rounded-lg max-h-40 md:max-h-36">
                 </div>
                 <div class="w-full pl-3">
-                    <div class="text-sm md:text-base md:pb-1 text-primary">Pemrograman</div>
-                    <h2 class="text-base md:text-xl font-bold">Judul Berita 1</h2>
-                    <p class="text-base md:text-lg md:pb-2">Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    <span
+                        class="bg-blue-100 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded md:mb-3">Pemrograman</span>
+                    <h2 class="text-2xl md:text-3xl font-bold">Judul Berita 1</h2>
+                    <p class="text-sm md:text-base  md:pb-3 text-green-800">Lorem ipsum, dolor sit amet consectetur
+                        adipisicing elit.
                         Repudiandae
                         ratione
                         accusantium vel? Autem assumenda eius nobis dolorem quos voluptates tempora.</p>
-                    <div class="text-sm text-primary">Dipublikasikan pada 01 Januari 2024</div>
+                    <div class="text-sm text-primary ">7 menit yang lalu
+                    </div>
                 </div>
             </div>
 
             <div class="flex mb-6">
                 <div class="w-1/3">
                     <img src="/assets/images/helloTech.webp" alt="News Image 1"
-                        class="object-cover w-full rounded-t-lg max-h-40 md:max-h-36">
+                        class="object-cover w-full rounded-lg max-h-40 md:max-h-36">
                 </div>
                 <div class="w-full pl-3">
-                    <div class="text-sm md:text-base md:pb-1 text-primary">Pemrograman</div>
-                    <h2 class="text-base md:text-xl font-bold">Judul Berita 1</h2>
-                    <p class="text-base md:text-lg md:pb-2">Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    <span
+                        class="bg-blue-100 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded md:mb-3">Pemrograman</span>
+                    <h2 class="text-2xl md:text-3xl font-bold">Judul Berita 1</h2>
+                    <p class="text-sm md:text-base md:pb-3 text-green-800">Lorem ipsum, dolor sit amet consectetur
+                        adipisicing elit.
                         Repudiandae
                         ratione
                         accusantium vel? Autem assumenda eius nobis dolorem quos voluptates tempora.</p>
-                    <div class="text-sm text-primary">Dipublikasikan pada 01 Januari 2024</div>
+                    <div class="text-sm text-primary ">7 menit yang lalu
+                    </div>
                 </div>
             </div>
+
+
+
         </x-elements.section_template>
     </section>
 
+    {{-- other --}}
+    <div class="absolute bottom-12 hidden md:block right-3 w-[200px] h-[200px]"
+        style="background-image: url(/assets/elements/circleDot.svg);">
+    </div>
+    <div class="absolute bottom-36 hidden md:block right-12 w-[200px] h-[200px]"
+        style="background-image: url(/assets/elements/circleDot.svg);">
+    </div>
 
 
 </x-layout_template>
