4d8a93ce391b5a386ec09287a90fe01f374518c2
MuhammadBobby <m.bobbyoktaviano@gmail.com>
2024-08-18 12:22:11 +0700

    Update: edit category proses

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -33,7 +33,7 @@ class CategoriesController extends Controller
     {
         $request->validate([
             'name' => 'required|string|unique:categories,name|min:2',
-            'image' => 'image|mimes:jpeg,png,jpg,svg,webp|max:2048',
+            'image' => 'required|image|mimes:jpeg,png,jpg,svg,webp|max:2048',
             'color' => 'required|string',
             'description' => 'required|string|min:3'
         ]);
@@ -82,14 +82,14 @@ class CategoriesController extends Controller
 
         // Membuat slug dari title
         $slug = Str::slug($request->name, '-');
-
+        $imageName = '';
         // Periksa apakah ada file image yang baru di-upload
         if ($request->hasFile('image')) {
             // Generate nama unik untuk file baru
             $newImageName = $category->slug . '-' . time() . '.' . $request->image->extension();
 
             // Hapus image lama jika ada
-            if ($category->image && file_exists(public_path('assets/category/' . $category->logo))) {
+            if ($category->logo && file_exists(public_path('assets/category/' . $category->logo))) {
                 unlink(public_path('assets/category/' . $category->logo));
             }
 
@@ -100,6 +100,7 @@ class CategoriesController extends Controller
         } else {
             $imageName = $category->image;
         }
+
 
         Category::where('slug', $category->slug)->update([
             'name' => $request->name,
